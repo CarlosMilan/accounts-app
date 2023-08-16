ce339e314d7254f81c1ff6617373a3e73189e6e3
Martin Milan <martin.milan3@gmail.com>
2023-08-16 19:38:21 -0300

    Fix

---
+++ accounts-auth-server/src/main/java/com/mm/accounts/authserver/AccountsAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.mm.accounts.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsAuthServerApplication.class, args);
+	}
+
+}
