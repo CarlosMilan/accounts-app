6272cbc42ba62138debfcd9cf42a265e931dc862
Martin Milan <martin.milan3@gmail.com>
2023-08-17 10:05:24 -0300

    Se agrega proyecto user service inicial

---
+++ user-service/src/test/java/com/mm/accounts/users/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.accounts.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
